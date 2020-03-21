08d26ae7478da77dd6b584d3c975f85843352932
Douglas Gatti <douglasbgatti@gmail.com>
2020-03-21 16:56:31 -0300

    Restructure app component with antd layout and content

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from "react";
 
 import { Provider } from "react-redux";
 
-import store from './redux/store';
+import store from "./redux/store";
 
 import "./App.css";
-
 import CakeContainer from "./components/CakeContainer";
+
+import { Layout } from "antd";
+
+const { Content } = Layout;
 
 const App = () => {
   let title = "React Redux App demo";
 
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1>{title}</h1>
+      <Layout>
+        <Content>
+          <div className="App">
+            <h1>{title}</h1>
 
-        <CakeContainer />
-      </div>
+            <CakeContainer />
+          </div>
+        </Content>
+      </Layout>
     </Provider>
   );
 };
