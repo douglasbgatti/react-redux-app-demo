ee16a04cd861f67e20aca6345fe1281c2f0277da
Douglas Gatti <douglasbgatti@gmail.com>
2020-03-21 16:56:09 -0300

    Connect redux state to cake container component

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,13 +1,36 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
 import { Button, Row } from "antd";
 
-const CakeContainer = () => {
-  return(
-      <Row justify="center">
-          <Button type="primary">Buy Cake</Button>
+import actions from "../redux/index";
+
+const CakeContainer = props => {
+  return (
+    <div>
+      <Row justify="center" gutter={16}>
+        <h2>Number of Cakes: {props.numOfCakes}</h2>
       </Row>
-  )
+      <Row justify="center" gutter={16}>
+        <Button type="primary" onClick={props.buyCake}>
+          Buy Cake
+        </Button>
+      </Row>
+    </div>
+  );
 };
 
-export default CakeContainer;
+const mapStateToProps = state => {
+  return {
+    numOfCakes: state.numOfCakes
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: () => dispatch(actions.cakeActions.buyCake())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
---
+++ src/redux/index.js
@@ -0,0 +1,5 @@
+import {actions as cakeActions} from './cakes/cakeActions';
+
+export default {
+    cakeActions
+}
