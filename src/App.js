b2ab74738137dc3d6391a6cfc2f9f1c8518a3c1f
Douglas Gatti <douglasbgatti@gmail.com>
2020-03-21 16:06:02 -0300

    Create initial react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  let title = "React Redux App demo";
+
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
